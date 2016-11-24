30d8d02bf34948597406a1ad0cb23ca04098dca0
sunil-mane <sunil_mane@yahoo.com>
2016-11-24 10:34:49 +0530

    localization changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, Http } from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {TranslateModule, TranslateLoader, TranslateStaticLoader} from 'ng2-translate';
 
 import { AppComponent } from './app.component';
+import { I18nComponent } from './components/i18n/i18n.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    I18nComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    TranslateModule.forRoot({
+            provide: TranslateLoader,
+            useFactory: (http: Http) => new TranslateStaticLoader(http, '/assets/i18n', '.json'),
+            deps: [Http] 
+        })
   ],
   providers: [],
   bootstrap: [AppComponent]
