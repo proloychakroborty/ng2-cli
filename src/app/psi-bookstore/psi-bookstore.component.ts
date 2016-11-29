3e46adf298ee80bd893adfafafe516dbb388308c
sunil-mane <sunil_mane@yahoo.com>
2016-11-29 16:57:45 +0530

    Angular 2 practice code

--- src/app/psi-bookstore/psi-bookstore.component.ts
+++ src/app/psi-bookstore/psi-bookstore.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PsiBookstoreComponent implements OnInit {
 
+  books: book[];
   constructor() { }
 
   ngOnInit() {
+    this.books = [{ author:"Sunil", pages:100 },{ author: "Proloy", pages:150 },{ author: "Abhijeet", pages:200 }];
   }
 
+}
+
+interface book {
+  author : string;
+  pages : number;
 }
