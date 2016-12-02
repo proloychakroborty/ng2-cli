3f01bced5b64a87b01d13d5d374c153d9b9957e4
sunil-mane <sunil_mane@yahoo.com>
2016-12-02 10:53:08 +0530

    Create booking

--- src/app/about-psi/about-psi.component.ts
+++ src/app/about-psi/about-psi.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BookingService } from '../shared/services/booking.service';
-import * as model from "../shared/model/model.d";
+import * as model from "../shared/model/model";
 
 @Component({
   selector: 'app-about-psi',
@@ -10,14 +10,22 @@ import * as model from "../shared/model/model.d";
 export class AboutPsiComponent implements OnInit {
 
   data: model.BookingRequest;
-  tests:any;
+  tests:model.BookingResponse;
 
   constructor(private bookingService: BookingService) {
 
    }
 
   ngOnInit() {
-    this.data = {};
+    this.data = {
+      applicationId: '2323',
+      clientId: 'abc1234',
+      candidateDetails: {},
+      eligibilityStartDate: '02-12-2016',
+      eligibilityEndDate: '10-12-2016',
+      testId: 'testid1',
+      testPortions: []
+    };
   }
 
   createBooking(){
